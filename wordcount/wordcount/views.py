b3819aeb1f78b111fec0c4c02964cb0cd57164d7
arlenemariano <arlene.codictted@gmail.com>
2019-11-15 13:59:32 +0800

    wordcount day 2

--- wordcount/wordcount/views.py
+++ wordcount/wordcount/views.py
@@ -1,4 +1,25 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("<h1>I love coffee!</h1>")
+    return render(request, "home.html")
+
+def by_value(el):
+    return el[1]
+
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+    count = len(wordlist)
+
+    worddictionary = {}
+    for word in wordlist:
+        if word in worddictionary:
+            worddictionary[word] += 1
+        else:
+            worddictionary[word] = 1
+
+    sortedwords = sorted(worddictionary.items(), key=by_value, reverse=True)
+
+    return render(request, "count.html", {"fulltext": fulltext, "count": count, "sortedwords": sortedwords})
